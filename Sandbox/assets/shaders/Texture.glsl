5ecf33d78ebc98a5909f2cfbe7412a09bfc19f22
Marian Bielcik <marian.bielcik@gmail.com>
2020-04-17 14:14:19 +0200

    Merged with upstream master TheCherno/Hazel

--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -1,7 +1,8 @@
 // Basic Texture Shader
 
 #type vertex
-#version 330 core
+//#version 330 core
+#version 450 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
@@ -26,7 +27,8 @@ void main()
 }
 
 #type fragment
-#version 330 core
+//#version 330 core
+#version 450 core
 
 layout(location = 0) out vec4 color;
 
